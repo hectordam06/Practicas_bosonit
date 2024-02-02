910272021ec3e06c0c9cacc9627cb19ae5f2cbb7
Hector Jimenez <hector.jimenez@bosonit.com>
2024-02-02 10:34:49 +0100

    commit ejercio perfiles1

---
+++ block5-profilesV2/src/test/java/com/bosonit/block5profilesV2/Block5ProfilesV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.block5profilesV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block5ProfilesV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
