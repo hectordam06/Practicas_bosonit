c29b05a77138ff7e81572c11461b27d138293fba
Hector Jimenez <hector.jimenez@bosonit.com>
2024-02-06 10:04:49 +0100

    MPrimer commit

---
+++ block6-simple-controllers/src/main/java/com/example/block6simplecontrollers/Controller.java
@@ -0,0 +1,17 @@
+package com.example.block6simplecontrollers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller {
+    @GetMapping("/user/{nombre}")
+    public String getUser(@PathVariable String nombre) {
+        return "Hola " + nombre;
+    }
+
+    @PostMapping("/useradd")
+    public Persona addUser(@RequestBody Persona persona) {
+        persona.setEdad(persona.getEdad() + 1);
+        return persona;
+    }
+}
---
+++ block6-simple-controllers/src/main/java/com/example/block6simplecontrollers/Persona.java
@@ -0,0 +1,16 @@
+package com.example.block6simplecontrollers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Persona {
+    private String nombre;
+    private String poblacion;
+    private int edad;
+}
