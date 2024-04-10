91a08c7957a1753f4fc9c2b08d0aff2c2a8f8854
Hector Jimenez <hector.jimenez@bosonit.com>
2024-04-10 09:28:58 +0200

    MEjercicio mensajeria con kafka

---
+++ BackWeb_TPV/src/main/java/com/bosonit/BackWeb_TPV/BackWebTpvApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.BackWeb_TPV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackWebTpvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackWebTpvApplication.class, args);
+	}
+
+}
